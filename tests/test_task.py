c169f1674a06c40a28632b6d76f5a015bf238e92
vexatom <237982002+vexatom@users.noreply.github.com>
2026-07-16 18:45:13 +0500

    test: add task fixtures

--- tests/test_task.py
+++ tests/test_task.py
@@ -3,30 +3,22 @@ import pytest
 from src.task_manager import Task
 
 
-def test_task_returns_correct_name():
-    name = "<NAME>"
-    description = "This is a test task"
+@pytest.fixture
+def task():
+    return Task('<NAME>', 'This is a test task')
 
-    task = Task(name, description)
+@pytest.fixture
+def inactive_task():
+    return Task('<NAME>', 'This is a test task', status=False)
 
-    assert task.name == name
+def test_task_returns_correct_name(task):
+    assert task.name == '<NAME>'
 
-def test_task_returns_correct_description():
-    name = "<NAME>"
-    description = "This is a test task"
+def test_task_returns_correct_description(task):
+    assert task.description == 'This is a test task'
 
-    task = Task(name, description)
-
-    assert task.description == description
-
-def test_task_returns_correct_status_when_status_is_false():
-    name = "<NAME>"
-    description = "This is a test task"
-    status = False
-
-    task = Task(name, description, status=status)
-
-    assert task.status == status
+def test_task_returns_correct_status_when_status_is_false(inactive_task):
+    assert inactive_task.status is False
 
 def test_task_raises_value_error_when_name_is_empty():
     name = ""
